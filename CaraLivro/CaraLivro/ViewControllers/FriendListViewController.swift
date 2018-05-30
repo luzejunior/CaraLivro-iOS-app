2139de2f963cd54b213b3b0a5a4813ed8e63a30d
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2018-05-30 01:35:08 -0300

    Coordinator modifications and friendlist viewController added

---
+++ CaraLivro/CaraLivro/ViewControllers/FriendListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FriendListViewController.swift
+//  CaraLivro
+//
+//  Created by Luzenildo Junior on 30/05/18.
+//  Copyright © 2018 Luzenildo Junior. All rights reserved.
+//
+
+import Foundation
