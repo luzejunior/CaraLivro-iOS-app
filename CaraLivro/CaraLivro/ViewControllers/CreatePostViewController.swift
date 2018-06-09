c065e40642092ef19bcabf0085ea9f54f70e9cb6
Luan Lima <luanlima@Luan-Macbook.local>
2018-06-09 13:56:08 -0300

    Add Imagem button in Create Post remade

--- CaraLivro/CaraLivro/ViewControllers/CreatePostViewController.swift
+++ CaraLivro/CaraLivro/ViewControllers/CreatePostViewController.swift
@@ -22,15 +22,29 @@ final class CreatePostViewController: UIViewController, UIImagePickerControllerD
     var image: UIImage?
     var currentMuralUserID: Int?
     var coordinator: CreatePostViewControllerActions?
+    var bottomConstraint: NSLayoutConstraint?
+    
+    let addImagemInputContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    
+    let addImagemButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Adicionar Imagem", for: .normal)
+        button.addTarget(self, action: #selector(openPhotoLibraryButton), for: .touchUpInside)
+        return button
+    }()
     
     var url = ""
     
-    @IBAction func openPhotoLibraryButton(sender: Any) {
+    @objc func openPhotoLibraryButton() {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = .photoLibrary;
-            imagePicker.allowsEditing = true
+            imagePicker.allowsEditing = false
             self.present(imagePicker, animated: true, completion: nil)
         }
     }
@@ -56,8 +70,50 @@ final class CreatePostViewController: UIViewController, UIImagePickerControllerD
         inputText.placeholder = "Digite seu texto"
         let button1 = UIBarButtonItem(title: "Postar", style: .plain, target: self, action: #selector(self.postButtonAction))
         self.navigationItem.rightBarButtonItem  = button1
+        
+        view.addSubview(addImagemInputContainerView)
+        view.addConstraintsWithFormat(format: "H:|[v0]|", views: addImagemInputContainerView)
+        view.addConstraintsWithFormat(format: "V:[v0(48)]", views: addImagemInputContainerView)
+        
+        bottomConstraint = NSLayoutConstraint(item: addImagemInputContainerView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        view.addConstraint(bottomConstraint!)
+        
+        setupInputComponents()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
+    @objc func handleKeyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+            let isKeyboardShowing = notification.name == NSNotification.Name.UIKeyboardWillShow
+            
+            bottomConstraint?.constant = isKeyboardShowing ? -keyboardFrame!.height : 0
+            
+            UIView.animate(withDuration: 0, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+                self.view.layoutIfNeeded()
+            }, completion: { (completed) in
+                // Ajustar a tableview quando o textField for selecionado.
+            })
+        }
+    }
+    
+    func setupInputComponents() {
+        let topBorderView = UIView()
+        topBorderView.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        
+        addImagemInputContainerView.addSubview(addImagemButton)
+        addImagemInputContainerView.addSubview(topBorderView)
+        
+        addImagemInputContainerView.addConstraintsWithFormat(format: "H:|-8-[v0]|", views: addImagemButton)
+        
+        addImagemInputContainerView.addConstraintsWithFormat(format: "V:|[v0]|", views: addImagemButton)
+        
+        addImagemInputContainerView.addConstraintsWithFormat(format: "H:|[v0]|", views: topBorderView)
+        addImagemInputContainerView.addConstraintsWithFormat(format: "V:|[v0(0.5)]", views: topBorderView)
+    }
+    
     @objc func postButtonAction() {
         let post = PostInUserMural(user_id_poster: currentUserInUse?.idUserProfile ?? 0, visibility: "public", text: inputText.text)
         let stringURL = "user/" + String(describing: currentMuralUserID ?? 0) + "/mural/post"
